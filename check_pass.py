2d9f82e33593b6870a7606948c28b13d9090e232
ram <24ram46r@gmail.com>
2023-08-05 19:23:52 +0530

    Automated Push after test

---
+++ check_pass.py
@@ -0,0 +1,23 @@
+import csv
+import os
+
+
+def execute():
+    print("Automating...")
+    os.system('git add .')
+    print("Added files to branch")
+    os.system('git commit -m "Automated Push after test"')
+    print("Commited files")
+    os.system('git push origin testing:deployment')
+    print("Pushed code to deployment branch")
+
+with open ("sqrt.csv", mode = 'r') as file:
+    pass_check = csv.reader(file)
+    for i in pass_check: 
+        if i != 'failed':
+            print("Executing")
+            execute()
+
+
+
+
--- test_sqrt.py
+++ test_sqrt.py
@@ -6,6 +6,10 @@ def test_square_positive():
 
 def test_square_decimal():
     b = round(squareroot(30),2)
-    assert b == 5.47
+    assert b == 5.48
+
+def test_square_zero():
+    b = squareroot(0)
+    assert b == 0.0 or 0
 
  
